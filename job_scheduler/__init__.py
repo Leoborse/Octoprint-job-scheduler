2648863d9ab9c623e66e685d5731b127ed576390
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-10 21:36:43 +0100

    Sperimentazione

--- job_scheduler/__init__.py
+++ job_scheduler/__init__.py
@@ -4,11 +4,14 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class OctoprintJobSchedulerPlugin(
-            octoprint.plugin.StartupPlugin,
-            octoprint.plugin.TemplatePlugin
-            ):
+        octoprint.plugin.StartupPlugin,
+        octoprint.plugin.TemplatePlugin,
+        octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Job-Scheduler!")
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
 __plugin_name__ = "Job-Scheduler"
 __plugin_implementation__ = OctoprintJobSchedulerPlugin()
