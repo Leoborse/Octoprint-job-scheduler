bd4ade5a44cf9e6799bcbdc9eb1ad1bbf19d5cd6
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-10 21:06:48 +0100

    Sperimentazione

--- job_scheduler/__init__.py
+++ job_scheduler/__init__.py
@@ -3,13 +3,11 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class OctoprintJobSchedulerPlugin(octoprint.plugin.StartupPlugin):
+class OctoprintJobSchedulerPlugin(
+            octoprint.plugin.StartupPlugin,
+            octoprint.plugin.TemplatePlugin
+            ):
     def on_after_startup(self):
         self._logger.info("Job-Scheduler!")
 
-__plugin_name__ = "Job-Scheduler"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "A job scheduler for OctoPrint"
 __plugin_implementation__ = OctoprintJobSchedulerPlugin()
-__plugin_author__ = "Leonardo Borselli"
-__plugin_url__ = "https://https://github.com/Leoborse/Octoprint-job-scheduler"
