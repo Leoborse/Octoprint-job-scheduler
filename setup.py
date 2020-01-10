12f029e334ee491ca77c7057581a9de24dc70667
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-10 21:58:33 +0100

    Sperimentazione

--- setup.py
+++ setup.py
@@ -75,3 +75,22 @@ if len(additional_setup_parameters):
         setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
+
+
+
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class OctoprintJobSchedulerPlugin(
+        octoprint.plugin.StartupPlugin,
+        octoprint.plugin.TemplatePlugin,
+        octoprint.plugin.SettingsPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+__plugin_name__ = "Job-Scheduler"
+__plugin_implementation__ = OctoprintJobSchedulerPlugin()
