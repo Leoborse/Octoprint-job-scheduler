0474c8d23d7b1022b00ce58d6d4986c550fcd09c
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-10 19:10:20 +0100

    Tentativo

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 plugin_identifier = "job_scheduler"
 plugin_package = "job_scheduler"
 plugin_name = "Job-Scheduler"
-plugin_version = "0.0.0"
+plugin_version = "0.0.1"
 plugin_description = """A job scheduler for OctoPrint"""
 plugin_author = "Leonardo Borselli"
 plugin_author_email = "leonardo.borselli@gmail.com"
-plugin_url = "https://https://github.com/Leoborse/Octoprint-job-scheduler"
+plugin_url = "https://github.com/Leoborse/Octoprint-job-scheduler"
 plugin_license = "AGPLv3"
 
 
