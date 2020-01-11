1d115eca2f04d2cbac1bdf7b0d1728568575b744
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-11 17:24:33 +0100

    Test navbar

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 plugin_identifier = "jobscheduler"
 plugin_package = "jobscheduler"
 plugin_name = "Job-Scheduler"
-plugin_version = "0.1.0"
+plugin_version = "0.1.1"
 plugin_description = """A job scheduler for OctoPrint"""
 plugin_author = "Leonardo Borselli"
 plugin_author_email = "leonardo.borselli@gmail.com"
