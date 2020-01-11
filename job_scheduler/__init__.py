8329080b3edb8e9578b6c313018bc99d2797e904
Leonardo Borselli <leonardo.borselli@gmail.com>
2020-01-11 08:52:55 +0100

    Test

--- job_scheduler/__init__.py
+++ job_scheduler/__init__.py
@@ -8,18 +8,22 @@ class JobSchedulerPlugin(
         octoprint.plugin.TemplatePlugin,
         octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+        self._logger.info("%s! (more: %s)" % self._settings.get(["name"]),self._settings.get(["url"]))
 
     def get_settings_defaults(self):
-        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+        return dict(
+            url="https://en.wikipedia.org/wiki/Hello_world",
+            name=__plugin_name__
+        )
+
+
+
 
 def get_implementation_class():
 	return JobSchedulerPlugin()
 
-
 __plugin_name__ = "Job-Scheduler"
 __plugin_implementation__ = get_implementation_class()
-__plugin_hooks__ = {
+__plugin_hooks__ = {}
 #	"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
 #	"octoprint.server.http.routes": __plugin_implementation__.route_hook
-}
